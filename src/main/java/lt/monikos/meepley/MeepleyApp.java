4d16119ce0ee3ab47db204417fa1f7469cc467f7
mkreive <mkreive@gmail.com>
2023-07-24 13:49:47 +0300

    initial

---
+++ src/main/java/lt/monikos/meepley/MeepleyApp.java
@@ -0,0 +1,13 @@
+package lt.monikos.meepley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeepleyApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeepleyApp.class, args);
+	}
+
+}
