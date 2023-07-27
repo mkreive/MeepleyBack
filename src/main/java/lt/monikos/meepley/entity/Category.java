435b7bccb3f7fd5b2073a122b2220bd5a4d4974a
mkreive <mkreive@gmail.com>
2023-07-27 10:38:21 +0300

    basic settup

---
+++ src/main/java/lt/monikos/meepley/entity/Category.java
@@ -0,0 +1,3 @@
+package lt.monikos.meepley.entity;
+
+public enum Category { STRATEGY, THEMATIC, FAMILY, ABSTRACT, CAMPAIGN, DECKBIULDER, DEXTERITY, EURO, ROLLWRITE, WARGAME}
---
+++ src/main/java/lt/monikos/meepley/entity/Game.java
@@ -0,0 +1,51 @@
+package lt.monikos.meepley.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "games")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "designer")
+    private String designer;
+
+    @Column(name = "publisher")
+    private String publisher;
+
+    @Column(name = "intro")
+    private String intro;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private int copies;
+
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+
+    @Column(name = "category")
+    private Category category;
+
+    @Column(name = "players")
+    private String players;
+
+    @Column(name = "rating")
+    private int rating;
+
+    @Column(name = "playing_time")
+    private String playingTime;
+
+    @Column(name = "img")
+    private byte[] img;
+}
