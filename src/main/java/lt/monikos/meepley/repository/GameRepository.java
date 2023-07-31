ac9737d64d2a4d1c68abd4f7ee6ec4d370a614e1
mkreive <mkreive@gmail.com>
2023-07-31 19:47:56 +0300

    find games repo redone

--- src/main/java/lt/monikos/meepley/repository/GameRepository.java
+++ src/main/java/lt/monikos/meepley/repository/GameRepository.java
@@ -2,19 +2,21 @@ package lt.monikos.meepley.repository;
 
 import lt.monikos.meepley.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 
-    List<Game> findByTitleContaining(@RequestParam("title") String title);
-
-    List<Game> findByCategory(@RequestParam("category") String category);
-
-    List<Game> findByComplexity(@RequestParam("complexity") String complexity);
-
-    List<Game> findByCategoryAndComplexity(@RequestParam("category") String category, @RequestParam("complexity") String complexity);
+    @Query("SELECT g FROM Game g " +
+            "WHERE " +
+            "(:category IS NULL OR g.category IN(:category)) " +
+            "AND " +
+            "(:complexity IS NULL OR g.complexity IN(:complexity)) " +
+            "AND " +
+            "(:title IS NULL OR g.title LIKE LOWER(CONCAT('%', :title,'%')))")
+    List<Game> findGames(@RequestParam("category") String category, @RequestParam("complexity") String complexity, @RequestParam("title") String title);
 
 
 }
