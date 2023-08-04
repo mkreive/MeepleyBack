0c69859e6c3b4cd77f1eb805aa8a836c6548b2ed
mkreive <mkreive@gmail.com>
2023-08-04 11:49:23 +0300

    sql selectors refactored

--- src/main/java/lt/monikos/meepley/repository/GameRepository.java
+++ src/main/java/lt/monikos/meepley/repository/GameRepository.java
@@ -11,12 +11,12 @@ public interface GameRepository extends JpaRepository<Game, Long> {
 
     @Query("SELECT g FROM Game g " +
             "WHERE " +
-            "(:category IS NULL OR g.category IN(:category)) " +
+            "(:#{null eq #category} = true  OR g.category IN :category) " +
             "AND " +
-            "(:complexity IS NULL OR g.complexity IN(:complexity)) " +
+            "(:#{null eq #complexity} = true OR g.complexity IN :complexity) " +
             "AND " +
             "(:title IS NULL OR g.title LIKE LOWER(CONCAT('%', :title,'%')))")
-    List<Game> findGames(@RequestParam("category") String category, @RequestParam("complexity") String complexity, @RequestParam("title") String title);
+    List<Game> findGames(@RequestParam("category") List<String> category, @RequestParam("complexity") List<String> complexity, @RequestParam("title") String title);
 
 
 }
